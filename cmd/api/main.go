30e2fc9dc73b4189e6516d39fd7f10fa05e80671
shindonghwi <ehdgnl8940@naver.com>
2022-06-15 16:31:19 +0900

    feat: json return 기능 추가

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag" // flag는 cmd에서 옵션을 지정할 수 있는 방법 중의 하나임.
 	"fmt"
 	"log"
@@ -14,6 +15,12 @@ type config struct {
 	env  string
 }
 
+type AppStatus struct {
+	Status      string // 상태
+	Environment string // 환경 ( dev, prod )
+	Version     string // 현재 버전
+}
+
 func main() {
 
 	var cfg config
@@ -24,13 +31,27 @@ func main() {
 
 	fmt.Println("Running: ", cfg.port, cfg.env)
 
+	// HandleFunc ( 수신할 경로, 핸들러 함수, http 포인터 )
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "status")
+		// fmt.Fprint(w, "status")
+		currentStatus := AppStatus{
+			Status:      "Available",
+			Environment: cfg.env,
+			Version:     version,
+		}
+
+		js, err := json.MarshalIndent(currentStatus, "", "'\t")
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(js)
 	})
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
-
-	if err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil); err != nil {
 		log.Println(err)
 	}
 }
