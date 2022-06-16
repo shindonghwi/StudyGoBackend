4af69e1922563b4febdf8c7cb0cf9e7f3da85d8d
shindonghwi <ehdgnl8940@naver.com>
2022-06-16 18:43:45 +0900

    feat: openDB function 제작

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"backend/models"
+	"context"
 	"database/sql"
 	"flag" // flag는 cmd에서 옵션을 지정할 수 있는 방법 중의 하나임.
 	"fmt"
@@ -41,10 +42,17 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
+	flag.StringVar(&cfg.db.dsn, "dsn", "ehdgnl8940:ehdgnl8940!@tcp(52.12.181.219:3306)/Wolf", "Mysql Connection String")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	db, err := sql.Open("mysql", "ehdgnl8940:ehdgnl8940!@tcp(52.12.181.219:3306)/Wolf")
+
+	db, err := openDB(cfg)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -65,15 +73,26 @@ func main() {
 	logger.Println("Starting server on port", cfg.port)
 
 	err = srv.ListenAndServe()
-
 	if err != nil {
 		log.Println(err)
 	}
+}
 
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}(db)
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
